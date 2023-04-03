c3b3bc47b85c7b52237a310461c620dc954cb006
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-04-03 08:50:01 +0200

    +1

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,24 +4,28 @@
 '''
 
 
+boxes = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
+
 def canUnlockAll(boxes):
     count = len(boxes)
     v = []
     r = range(1, count)
     for i in range(count):
         for j in boxes[i]:
-            if j != i:
                 if j not in v:
-                    v.append(j)
+                    if j != i:
+                        v.append(j)
                     if 0 in v:
                         v.remove(0)
                         v.sort()
-            else:
-                return False
     k = 1
     while k < count - 1:
         k += 1
     if k not in v:
+        print('false')
         return False
     else:
+        print('true')
         return True
+
+canUnlockAll(boxes)
