bb953d39582de3b66bdc1fe357733094771376bb
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-04-03 13:03:54 +0200

    +1

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -9,17 +9,17 @@
  */
 
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *node = NULL;
+node = malloc(sizeof(binary_tree_t));
 
-    binary_tree_t *node = NULL;
-    node = malloc(sizeof(binary_tree_t));
+if (!node)
+	return (NULL);
+node->n = value;
+node->left = NULL;
+node->right = NULL;
+node->parent = parent;
 
-    if (!node)
-		return (NULL);
-	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
-	node->parent = parent;
-
-	return (node);
+return (node);
 }
