732255b548e5cf804377cdf2a696e9aafbf5ff1c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-04-03 14:19:26 +0200

    +2

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * heap_insert - Prototype
+ * @root: is a double pointer to the root node of the Heap
+ * @value: is the value to store in the node to be inserted
+ * Return: a pointer to the inserted node, or NULL on failure
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+
+}
