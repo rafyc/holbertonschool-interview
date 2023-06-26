76270323b48a9f1bbfaba84451d831341d83ad42
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-26 13:22:12 +0200

    +0

---
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,24 @@
+#include <binary_trees.h>
+
+/**
+sorted_array_to_avl -function that builds an AVL tree from an array
+@array: pointer to the first element of the array to be converted
+@size: the number of element in the array
+*/
+
+binary_tree_t *create(binary_tree_t parent, int value)
+{
+    new = malloc(sizeof(binary_tree_t));
+    if !new
+        return (NULL);
+    new->parent = parent;
+    new->left = NULL;
+    new->right = NULL;
+    new->n = int
+    return (new);
+}
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+    int mid =
+}
