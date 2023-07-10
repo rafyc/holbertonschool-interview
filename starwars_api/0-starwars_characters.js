504cf2adfe07e99ca52108716a9c406a0bda22e8
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-10 09:59:13 +0200

    +1

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,34 +1,44 @@
 #!/usr/bin/node
-import fetch from "node-fetch";
+import request from 'request';
 const args = process.argv;
 
-const getChar = async () => {
-  try {
-    let res = await fetch(`https://swapi-api.hbtn.io/api/films/${args[2]}`)
-    let data = await res.json()
-    return data.characters;
-  } catch (error) {
-    console.log(error);
-  }
-}
+const getChar = () => {
+  return new Promise((resolve, reject) => {
+    request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const data = JSON.parse(body);
+        resolve(data.characters);
+      }
+    });
+  });
+};
 
-const getName = async (url) => {
-  try {
-    let res = await fetch(url)
-    let data = await res.json()
-    console.log(data.name);
-    return data.name;
-  } catch (error) {
-    console.log(error);
-  }
-}
+const getName = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const data = JSON.parse(body);
+        console.log(data.name);
+        resolve(data.name);
+      }
+    });
+  });
+};
 
 const printNames = async () => {
-  let arrName = await getChar();
+  try {
+    const arrName = await getChar();
 
-  arrName.forEach(async (element) => {
-    await getName(element);
-  });
+    for (const element of arrName) {
+      await getName(element);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 printNames();
