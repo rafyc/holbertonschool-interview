6b4996566bc068066fec0e084d0de3d0bee7c210
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-25 15:49:01 +0200

    1

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -7,8 +7,8 @@ def count_words(subreddit, word_list, word_dict={}, after=""):
     """This method requests the api reddit to return the number time that
     a word is counted in the title of all article for a given subreddit"""
     word_list = [ele.lower() for ele in word_list]
-    if not wordDict:
-        wordDict = {key: 0 for key in word_list}
+    if not word_dict:
+        word_dict = {key: 0 for key in word_list}
 
     user_agent = "Pleasant_Situation20"
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
@@ -25,11 +25,11 @@ def count_words(subreddit, word_list, word_dict={}, after=""):
         title = article_data.get('title', '').lower()
         for word in word_list:
             if (word in title):
-                wordDict[word] += 1
+                word_dict[word] += 1
                 if after is not None:
-                    return count_words(subreddit, word_list, after, wordDict)
+                    return count_words(subreddit, word_list, word_dict, after)
 
-    sorted_wordDict = dict(sorted(wordDict.items()))
+    sorted_wordDict = dict(sorted(word_dict.items()))
     for key, value in sorted_wordDict.items():
         if (value != 0):
             print(f"{key}: {value}")
