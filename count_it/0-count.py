1131b99cbdec37a5ed54024f65a02cfc36f96b5a
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-25 13:59:16 +0200

    1

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -15,7 +15,6 @@ def count_words(subreddit, word_list, after=None, counts=None):
     user_agent = "Pleasant_Situation20"
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': user_agent}
-    count = 0
     wordDict = {key: 0 for key in word_list}
     req = requests.get(url, headers=headers, allow_redirects=False)
     data = req.json().get('data')
@@ -23,7 +22,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
         article_data = ele.get('data', {})
         title = article_data.get('title', '').lower()
         for word in word_list:
-            if(word in title):
+            if (word in title):
                 wordDict[word] += 1
     for key, value in wordDict.items():
         print(f"{key}: {value}")
