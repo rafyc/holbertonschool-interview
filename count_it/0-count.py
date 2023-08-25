28ea13bd35ab9cb09a87fdf3b37424c0b1e6d6d7
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-25 15:06:15 +0200

    1

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -4,26 +4,33 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
+def count_words(subreddit, word_list, after='', wordDict={}):
     '''This method requests the api reddit to return the number time that
     a word is counted in the title of all article for a given subreddit
     '''
     word_list = [ele.lower() for ele in word_list]
+    wordDict = {key: 0 for key in word_list}
 
     user_agent = "Pleasant_Situation20"
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': user_agent}
-    wordDict = {key: 0 for key in word_list}
-    req = requests.get(url, headers=headers, allow_redirects=False)
+    params = {'after': after, 'limit': 100}
+
+    req = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
     data = req.json().get('data')
+    after = data.get('after')
+
     for ele in data.get('children', []):
         article_data = ele.get('data', {})
         title = article_data.get('title', '').lower()
         for word in word_list:
             if (word in title):
                 wordDict[word] += 1
+                if after is not None:
+                    return count_words(subreddit, word_list, after, wordDict)
 
     sorted_wordDict = dict(sorted(wordDict.items()))
-
     for key, value in sorted_wordDict.items():
-        print(f"{key}: {value}")
+        if (value != 0):
+            print(f"{key}: {value}")
