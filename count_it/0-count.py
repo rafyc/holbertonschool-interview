9d50d4d825b5b935789d25856ea34c8676a7d249
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-25 15:42:14 +0200

    1

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
-"""This module defines the count_words function"""
+'''This module defines the count_words function
+'''
 import requests
 
 
-def count_words(subreddit, word_list, word_dict={}, after=""):
-    """This method requests the api reddit to return the number time that
-    a word is counted in the title of all article for a given subreddit"""
-    lower_word_list = [word.lower() for word in word_list]
+def count_words(subreddit, word_list, after='', wordDict={}):
+    '''This method requests the api reddit to return the number time that
+    a word is counted in the title of all article for a given subreddit
+    '''
+    word_list = [ele.lower() for ele in word_list]
+    if not wordDict:
+        wordDict = {key: 0 for key in word_list}
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'elodieriou3685'}
-    params = {'after': after,
-              'limit': 100}
+    user_agent = "Pleasant_Situation20"
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': user_agent}
+    params = {'after': after, 'limit': 100}
 
-    req_reddit = requests.get(url,
-                              headers=headers,
-                              params=params,
-                              allow_redirects=False)
+    req = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
-    if req_reddit.status_code > 300:
-        return None
+    data = req.json().get('data')
+    after = data.get('after')
 
-    hot = req_reddit.json().get('data')
-    after = hot.get('after')
+    for ele in data.get('children', []):
+        article_data = ele.get('data', {})
+        title = article_data.get('title', '').lower()
+        for word in word_list:
+            if (word in title):
+                wordDict[word] += 1
+                if after is not None:
+                    return count_words(subreddit, word_list, after, wordDict)
 
-    for child in hot.get('children'):
-        titles = child.get('data').get('title')
-        lower_word_title = [title.lower() for title in titles.split(' ')]
-
-        for word in lower_word_list:
-            word_dict[word] = word_dict.setdefault(word, 0) + \
-                              lower_word_title.count(word)
-
-    if after is not None:
-        return count_words(subreddit, lower_word_list, word_dict, after)
-
-    sorted_word_dict = sorted(word_dict.items(), key=lambda w: (-w[1], w[0]))
-    for key, value in sorted_word_dict:
-        if value > 0:
-            print("{}: {}".format(key, value))
-
-    return word_dict
+    sorted_wordDict = dict(sorted(wordDict.items()))
+    for key, value in sorted_wordDict.items():
+        if (value != 0):
+            print(f"{key}: {value}")
