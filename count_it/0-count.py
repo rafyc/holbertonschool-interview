0f8520056b009c36819970cf80ec8abdcefc9054
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-30 11:43:44 +0200

    1

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -19,31 +19,30 @@ Returns:
 dict: A dictionary containing word occurrences.
 
 """
-    if not word_dict:
-        word_dict = {key: 0 for key in word_list}
+if not word_dict:
+    word_dict = {key: 0 for key in word_list}
 
-    user_agent = "Pleasant_Situation20"
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': user_agent}
-    params = {'after': after, 'limit': 100}
+user_agent = "Pleasant_Situation20"
+url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+headers = {'User-Agent': user_agent}
+params = {'after': after, 'limit': 100}
 
-    req = requests.get(url, headers=headers, params=params, allow_redirects=False)
+req = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
-    data = req.json().get('data')
-    after = data.get('after')
+data = req.json().get('data')
+after = data.get('after')
 
-    for ele in data.get('children', []):
-        article_data = ele.get('data', {})
-        title = article_data.get('title', '').lower()
-        for word in word_list:
-            if (word in title):
-                word_dict[word] += 1
-                if after is not None:
-                    return count_words(subreddit, word_list, word_dict, after)
+for ele in data.get('children', []):
+    article_data = ele.get('data', {})
+    title = article_data.get('title', '').lower()
+    for word in word_list:
+        if (word in title):
+            word_dict[word] += 1
+            if after is not None:
+                return count_words(subreddit, word_list, word_dict, after)
+sorted_wordDict = dict(sorted(word_dict.items()))
+for key, value in sorted_wordDict.items():
+    if (value != 0):
+        print(f"{key}: {value}")
 
-    sorted_wordDict = dict(sorted(word_dict.items()))
-    for key, value in sorted_wordDict.items():
-        if (value != 0):
-            print(f"{key}: {value}")
-
-    return sorted_wordDict
+return sorted_wordDict
